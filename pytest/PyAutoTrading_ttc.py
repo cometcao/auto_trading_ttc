3783ddeb4f5ba74007cffdfa283042dae0a5c520
MetalInvest <MetalInvest@DESKTOP-IARABV4>
2016-11-03 14:41:25 +1100

    change error handling

--- pytest/PyAutoTrading_ttc.py
+++ pytest/PyAutoTrading_ttc.py
@@ -22,22 +22,22 @@ class TTC_autoTrader:
     def orderProcess(self):
         orders = self.getOrderDetails()
         #orders = [('002755', 0, 10)]
-        for stock, pct, price in orders:
-            free_cash, _, total_value, _ = self.winapi.getAccDetails()
-            if pct == 0: # sell off the stock
-                self.winapi.clearStock(stock)
-            else:
-                self.winapi.adjustStock(stock, pct, price, free_cash, total_value)
-            time.sleep(3) # pause 5 seconds after each order
+        try:
+            for stock, pct, price in orders:
+                free_cash, _, total_value, _ = self.winapi.getAccDetails()
+                if pct == 0: # sell off the stock
+                    self.winapi.clearStock(stock)
+                else:
+                    self.winapi.adjustStock(stock, pct, price, free_cash, total_value)
+                time.sleep(3) # pause 5 seconds after each order
+        except:
+            traceback.print_exc()
+            raise "order failed! please check!"
             
 if __name__ == '__main__':
     print("Auto Trading system started/自动交易系统运行中， 请开启同花顺网上交易系统5.0")
     ttc = TTC_autoTrader()
     while True:
-        try:
-            print(strftime("%Y-%m-%d %H:%M:%S"))
-            ttc.orderProcess()
-        except:
-            traceback.print_exc()
-            raise "order failed! please check!"
-        time.sleep(60)
+        print(strftime("%Y-%m-%d %H:%M:%S"))
+        ttc.orderProcess()
+        time.sleep(180)
